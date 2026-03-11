79a9a242ff5adfced620c6690bdebe3be7624b78
DiegoRivas1 <rhyi888@gmail.com>
2026-03-10 23:32:17 -0500

    Creacion del modelo

---
+++ src/main/java/gm/empleados/modelo/Empleado.java
@@ -0,0 +1,35 @@
+package gm.empleados.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data //Get y set
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer idEmpleado;//id_empleado
+    String nombreEmpleado;
+    String departamento;
+    String sueldo;
+}
+
+
+
+
+
+
+
+
+
+
+
